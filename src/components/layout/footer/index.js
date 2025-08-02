814203404decf5501f32ef7dc7dc231af4a0a650
MaxRW <maxrw9@atomicmail.io>
2025-08-02 12:25:30 -0300

    Header and Footer 01

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -2,10 +2,13 @@ import { PAIZO_CUP } from "src/utils/globals";
 import "./footer.css";
 
 const Footer = () => {
+
+
     return (
         <div className='footer'>
-            <img className="Plogo" src="/P.png"/>
+            <div className="Plogo"><img src="/P.png"/></div>
             <PAIZO_CUP className='paizo-cup' />
+            <div className="footer-void"/>
         </div>
     );};
 
